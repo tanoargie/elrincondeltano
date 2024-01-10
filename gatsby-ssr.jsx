6b8ac3e48b8653f3932b2e3ba8d73c9ff42115e5
Franco Serio <franco@samser.co>
2024-01-09 22:54:50 -0300

    fix: Test using resolveUrl

--- gatsby-ssr.jsx
+++ gatsby-ssr.jsx
@@ -127,6 +127,29 @@ export const onRenderBody = ({ setHeadComponents }) => {
         gtag('js', new Date());
         gtag('config', ${process.env.GTAG}, { page_path: location ? location.pathname + location.search + location.hash : undefined })
       `}
-    </Script>
+    </Script>,
+    <script
+      key="partytown-vanilla-config"
+      dangerouslySetInnerHTML={{
+        __html: `partytown = {
+           resolveUrl(url, location) {
+              if (url.hostname.includes('google-analytics')) {
+                // Use a secure connection
+                if (url?.protocol === 'http:') {
+                  url = new URL(url.href.replace('http', 'https'))
+                }
+
+                // Point to our proxied URL
+                const proxyUrl = new URL(location.origin + '/__third-party-proxy')
+                proxyUrl.searchParams.append('url', url)
+
+                return proxyUrl
+              }
+
+              return url
+           }
+         }`,
+      }}
+    />,
   ])
 }
