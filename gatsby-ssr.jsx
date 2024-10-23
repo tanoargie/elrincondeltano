c39d62cfcef6f6e6d2120b874cf6a5631750c39d
Franco Serio <franco@samser.co>
2024-10-24 01:18:30 +0200

    fix: checking window.gtag

--- gatsby-ssr.jsx
+++ gatsby-ssr.jsx
@@ -3,14 +3,14 @@ import { Partytown } from '@builder.io/partytown/react';
 
 export const onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
-    <Partytown key="partytown" forward={['dataLayer.push']} />,
+    <Partytown key="partytown" forward={['gtag']} />,
     <script key="google-analytics" type="text/partytown" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTAG}`} />,
     <script
       key="google-analytics-config"
       type="text/partytown"
       dangerouslySetInnerHTML={{
         __html: `window.dataLayer = window.dataLayer || [];
-        function gtag(){ dataLayer.push(arguments);}
+        window.gtag = function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date()); 
         gtag('config', '${process.env.GTAG}')`
       }}
